384d886f9950e00eeb8376509d0c357390085ba7
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2026-05-09 15:55:56 +0200

    Setup navigation html

---
+++ src/scripts/app.js
@@ -0,0 +1,4 @@
+const $menu = document.querySelector(".menu");
+const $menuLinks = document.querySelectorAll(".menu__link");
+const $openBtn = document.querySelector(".nav__toggle");
+const $closeBtn = document.querySelector(".menu__close");
