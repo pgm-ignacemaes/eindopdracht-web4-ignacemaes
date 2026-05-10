2edb9d9da4b9c173d3d6b1e7435a22b38d009b94
«pgm-ignacemaes» <ignace.maes@student.arteveldehs.be>
2026-05-10 08:48:28 +0200

    Created hamburger icon animation

---
+++ src/scripts/animations/basic.js
@@ -0,0 +1,11 @@
+import gsap from "gsap";
+
+export const hamburgerLineTween = ($elements) => {
+  $elements.forEach(($element, index) => {
+    gsap.to($element, {
+      rotateZ: index % 2 === 0 ? 45 : -45,
+      y: index % 2 === 0 ? 3 : -3,
+      duration: 0.5,
+    });
+  });
+};
